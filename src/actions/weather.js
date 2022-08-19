c6aa8f00dbbcf08cc417f98deea91a079c454dbe
Emmanuelle Laguna <emmanuelle.laguna@gmail.com>
2022-08-19 13:38:41 -0500

    It worsk with API key from .env

--- src/actions/weather.js
+++ src/actions/weather.js
@@ -4,7 +4,7 @@ import api from "../api/api.json";
 
 const GET_WEATHER = "GET_WEATHER";
 
-const API_KEY = process.env.OPEN_WEATHER_API_KEY;
+const API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
 
 const get = (data) => ({
     type: GET_WEATHER,
@@ -15,3 +15,4 @@ export const getWeather = (cityName) => dispatch => {
     axios(`${api.base_url}?${api.params[0]}=${cityName}&${api.params[1]}=${API_KEY}&units=metric`)
         .then(response => dispatch(get(response.data)));
 };
+
