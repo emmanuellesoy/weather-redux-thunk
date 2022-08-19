bcfad7b88eaa5ee9abf4f079a175c138971328f3
Emmanuelle Laguna <emmanuelle.laguna@gmail.com>
2022-08-19 13:23:09 -0500

    ADD API KEY with .env

--- src/actions/weather.js
+++ src/actions/weather.js
@@ -4,12 +4,14 @@ import api from "../api/api.json";
 
 const GET_WEATHER = "GET_WEATHER";
 
+const API_KEY = process.env.OPEN_WEATHER_API_KEY;
+
 const get = (data) => ({
     type: GET_WEATHER,
     data
 });
 
 export const getWeather = (cityName) => dispatch => {
-    axios(`${api.base_url}?${api.params[0]}=${cityName}&${api.params[1]}=${api.key}&units=metric`)
+    axios(`${api.base_url}?${api.params[0]}=${cityName}&${api.params[1]}=${API_KEY}&units=metric`)
         .then(response => dispatch(get(response.data)));
 };
